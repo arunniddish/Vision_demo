81e5969c3e76796ef2c96efca2bb90f00d5221e5
arunniddish <63624213+arunniddish@users.noreply.github.com>
2021-03-13 17:07:24 -0600

    Add files via upload

---
+++ test_CF.m
@@ -0,0 +1,21 @@
+% define the device (either Arduino or Seeeduino) serial communication
+device = serialport("COM10",9600);
+
+% initialized gait cycle number
+    m = 0;
+    
+while(true)
+    % reads the gait cycle number as a char value
+    m_char = readline(device);
+    % converts to double
+    m = str2double(m_char);
+   
+     if(m>=2)
+         % defines new T_d as double, rounded to make sure it is an int
+        Td_int = round(m*1000);
+        % converts double to string 
+        Td = num2str(Td_int);
+        % writes string to serial port
+        writeline(device,Td);
+     end
+end
